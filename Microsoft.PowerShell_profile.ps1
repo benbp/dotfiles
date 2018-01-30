c0b333049a55d539e5eee8f72c105427d2f1537e
Ben Broderick Phillips <bebroder@microsoft.com>
2018-01-30 12:01:33 -0800

    git, prompt updates for powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,34 +1,56 @@
-# Add to C:\Users\<user>\Documents\WindowsPowerShell
-# for cmder, remove -NoProfile from the powershell startup task
-
 Import-Module PSReadLine
+
+$sheep = $(Get-Emoji 'SHEEP')
+
+[ScriptBlock]$PsPrompt = {
+    $realLastExitCode = $LASTEXITCODE
+
+    $host.UI.RawUI.WindowTitle = $pwd.ProviderPath
+    #$host.UI.RawUI.WindowTitle = Microsoft.PowerShell.Management\Split-Path $pwd.ProviderPath
+    Microsoft.PowerShell.Utility\Write-Host $sheep -NoNewLine -ForegroundColor "DarkGray"
+
+    $global:LASTEXITCODE = $realLastExitCode
+    return " "
+}
+Set-Item -Path function:\prompt  -Value $PsPrompt  -Options ReadOnly -force
+
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+
+function changedir($dir) {
+    if ($dir -eq "-") {
+        popd
+    } elseif ($dir -eq "~") {
+        pushd $HOME
+    } else {
+        pushd $dir
+    }
+}
+
+del alias:cd -Force
+Set-Alias cd changedir
 
 function acis { cd C:\Repos\EngSys\Acis\Legacy\src }
 function acisinit { cd C:\Repos\EngSys\Acis\Legacy; .\init.ps1 }
+function acisvis { cd C:\Repos\EngSys\Acis\Legacy\src; vsmsbuild dirs.proj }
+function acisinitvis { cd C:\Repos\EngSys\Acis\Legacy; .\init.ps1; cd .\src; vsmsbuild dirs.proj }
+function proto { cd C:\Repos\sqlproto\odatatools\odata-openapi }
+
+function cpf($f) { $env:CurrentFileYank=$(dir $f).FullName }
+function pf { cp $env:CurrentFileYank . }
 
 function prof { vim $PROFILE }
 function profile { vim $PROFILE }
 
-function gits { git status }
-function gitp { git push }
-function gitpf { git push -f }
-function gita { git add -u }
-function gitd { git diff --staged }
-function gitm($msg) { git commit -m $msg }
-function gitam($msg) { git add -u; git commit -m $msg }
-function gitl { git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit }
-function gitlg($n) { git log -n $n }
-function copybranch {
-    git branch | awk '/\*/ {print $2;}' | clip
-}
-function gitgrep($term) { git grep -i --color --break --heading --line-number $term }
-function gitrb($n) { git rebase -i HEAD~$n }
-
+if (Test-Path alias:gb) { del alias:gb -Force }
+if (Test-Path alias:gf) { del alias:gf -Force }
+if (Test-Path alias:gco) { del alias:gco -Force }
 if (Test-Path alias:gs) { del alias:gs -Force }
 if (Test-Path alias:gp) { del alias:gp -Force }
-if (Test-Path alias:gpf) { del alias:gpf -Force }
+if (Test-Path alias:gpl) { del alias:gpl -Force }
 if (Test-Path alias:ga) { del alias:ga -Force }
+if (Test-Path alias:gau) { del alias:gau -Force }
 if (Test-Path alias:gd) { del alias:gd -Force }
+if (Test-Path alias:gds) { del alias:gds -Force }
 if (Test-Path alias:gm) { del alias:gm -Force }
 if (Test-Path alias:gam) { del alias:gam -Force }
 if (Test-Path alias:gl) { del alias:gl -Force }
@@ -37,17 +59,42 @@ if (Test-Path alias:ggrep) { del alias:ggrep -Force  }
 if (Test-Path alias:grb) { del alias:grb -Force }
 if (Test-Path alias:cgb) { del alias:cgb -Force }
 
-Set-Alias gs gits
-Set-Alias gp gitp
-Set-Alias gpf gitpf
-Set-Alias ga gita
-Set-Alias gd gitd
-Set-Alias gm gitm
-Set-Alias gam gitam
-Set-Alias gl gitl
-Set-Alias glg gitlg
+function gitbranch { git branch $args }
+function gitfetch { git fetch --all }
+function gitcheckout { git checkout $args }
+function gitstatus { git status }
+function gitpush { git push $args }
+function gitpull { git pull }
+function gitadd { git add $args }
+function gitaddu { git add -u }
+function gitdiff { git diff }
+function gitdiffstaged { git diff --staged }
+function gitcommit { git commit -m $args }
+function gitaddcommit { git add -u; git commit -m $args }
+function gitlogpretty { git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit }
+function gitlognum($n) { git log -n $n }
+function copybranch {
+    git branch | awk '/\*/ {print $2;}' | clip
+}
+function gitgrep { git grep -i --color --break --heading --line-number $args }
+function gitrebase($n) { git rebase -i HEAD~$n }
+
+Set-Alias gco gitcheckout
+Set-Alias gb gitbranch
+Set-Alias gf gitfetch
+Set-Alias gs gitstatus
+Set-Alias gp gitpush
+Set-Alias gpl gitpull
+Set-Alias ga gitadd
+Set-Alias gau gitaddu
+Set-Alias gd gitdiff
+Set-Alias gds gitdiffstaged
+Set-Alias gm gitcommit
+Set-Alias gam gitaddcommit
+Set-Alias gl gitlogpretty
+Set-Alias glg gitlognum
 Set-Alias ggrep gitgrep
-Set-Alias grb gitrb
+Set-Alias grb gitrebase
 Set-Alias cgb copybranch
 
 Set-PSReadLineOption -EditMode Vi
@@ -55,6 +102,18 @@ Set-PSReadlineKeyHandler -Chord "j,k" -Function ViCommandMode
 
 Set-PSReadlineKeyHandler -Key Ctrl+r -Function ReverseSearchHistory
 Set-PSReadlineKeyHandler -Key Ctrl+k -Function ClearScreen
+
+Set-PSReadlineKeyHandler -Chord Ctrl+L -ScriptBlock { 
+    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine() 
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert('dir')
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+}
+
+Set-PSReadlineKeyHandler -Chord Ctrl+p -ScriptBlock { 
+    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine() 
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert('$(pwd).Path')
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+}
 
 # Searching for commands with up/down arrow is really handy.  The
 # option "moves to end" is useful if you want the cursor at the end
@@ -191,3 +250,4 @@ Set-PSReadlineOption -CommandValidationHandler {
         }
     }
 }
+
