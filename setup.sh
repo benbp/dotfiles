ea2fe3a5b425f470ae0900b21685d41e6304a78a
Ben Broderick Phillips <ben@benbp.net>
2021-02-27 19:43:54 +0000

    Update setup script

--- setup.sh
+++ setup.sh
@@ -8,10 +8,10 @@ sudo apt install -y apt-transport-https lsb-release dirmngr
 # Repositories
 
 # Azure
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" | \
-	sudo tee /etc/apt/sources.list.d/azure-cli.list
-sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv --keyserver packages.microsoft.com \
-	--recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
+#echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" | \
+#	sudo tee /etc/apt/sources.list.d/azure-cli.list
+#sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv --keyserver packages.microsoft.com \
+#	--recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
 # Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository  "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
@@ -22,11 +22,9 @@ sudo apt-get update
 sudo apt install -y apt-transport-https ca-certificates software-properties-common curl
 sudo apt install -y docker-ce
 sudo apt install -y tmux
-sudo snap install kubectl --classic
-sudo snap install helm --classic
 
 # Azure packages
-sudo apt install -y azure-cli
+#sudo apt install -y azure-cli
 
 # Go
 sudo snap install go --classic
@@ -43,21 +41,36 @@ wget https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/c
 echo "====== Update neovimrc link to vimrc ======="
 
 # Repos
-git config --global user.email "bebroder@microsoft.com"
+git config --global user.email "ben@benbp.net"
 git config --global user.name "Ben Broderick Phillips"
 git config --global core.editor vim
 git config --global core.autocrlf input
-read -p "Add aks vsts redirects to git config"
-read -p "Copy vsts pub/private keys to ~/.ssh/id_rsa.vsts[.pub]"
-eval `ssh-agent`
-ssh-add ~/.ssh/id_rsa.vsts
 
 # Dotfiles
 wget https://raw.githubusercontent.com/benbp/dotfiles/master/.inputrc -O ~/.inputrc
-wget https://raw.githubusercontent.com/benbp/dotfiles/master/.bashrc -O ~/.bashrc
+#wget https://raw.githubusercontent.com/benbp/dotfiles/master/.bashrc -O ~/.bashrc
+echo zsh > ~/.bashrc
 wget https://raw.githubusercontent.com/benbp/dotfiles/master/.vimrc -O ~/.vimrc
-wget https://raw.githubusercontent.com/benbp/dotfiles/master/.tmux.conf -O ~/.tmux.conf
+wget https://raw.githubusercontent.com/benbp/dotfiles/master/.antigenrc -O ~/.antigenrc
+wget https://raw.githubusercontent.com/benbp/dotfiles/master/.zshrc -O ~/.zshrc
+mkdir -p ~/.tmux
+wget https://raw.githubusercontent.com/benbp/dotfiles/master/.tmux/theme.conf -O ~/.tmux/theme.conf
+
+# term
+sudo apt-get install -y zsh
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+# for z.lua
+sudo apt-get install -y lua5.3
+sudo ln -s /usr/bin/lua5.3 /usr/bin/lua
+wget https://raw.githubusercontent.com/skywind3000/z.lua/master/z.lua -O ~/z.lua
 
 # neovim setup
 npm install -g neovim
+ln -sf /home/ben/.vim ~/.config/nvim
+ln -sf /home/ben/.vimrc ~/.config/nvim/init.vim
 # Do all coc.vim installs
+# in vim: :PlugInstall
+
+# tmux setup
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
